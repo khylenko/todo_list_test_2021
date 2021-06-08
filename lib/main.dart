4fc580c938bad6cfabd88ac52a0813911cbc23d4
Khylenko Ihor <i.khylenko@splinestudio.ua>
2021-06-08 22:20:21 +0300

    [task1] added and initialized database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:to_do_list_test/view/home_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('todos');
   runApp(MyApp());
 }
 
